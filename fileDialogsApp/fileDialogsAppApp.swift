e5f45ea36609a49d374e168b74803f5ac5b30660
Steven barnett <steve@sabarnett.co.uk>
2024-07-20 11:13:19 +0100

    Initial Commit

---
+++ fileDialogsApp/fileDialogsAppApp.swift
@@ -0,0 +1,22 @@
+//
+// -----------------------------------------
+// Original project: fileDialogsApp
+// Original package: fileDialogsApp
+// Created on: 20/07/2024 by: Steven Barnett
+// Web: http://www.sabarnett.co.uk
+// GitHub: https://www.github.com/sabarnett
+// -----------------------------------------
+// Copyright © 2024 Steven Barnett. All rights reserved.
+//
+    
+
+import SwiftUI
+
+@main
+struct fileDialogsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
